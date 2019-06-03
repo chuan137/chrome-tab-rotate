f52a1fce60221b574cad6dfeacd7b45c4c5720f8
Chuan Miao <chuan137@gmail.com>
2019-06-03 12:18:07 +0200

    rotate tabs

--- event.js
+++ event.js
@@ -1,17 +1,46 @@
+let session = newSessionObject();
 
-chrome.browserAction.onClicked.addListener(function(tab) { 
-    var ntabs;
+function newSessionObject() {
+    return {
+        isPlaying: false,
+        windowId: null,
+        timerId: null,
+        interval: 5,
+    }
+}
 
-    chrome.tabs.query({currentWindow: true}, function(t){
-        ntabs = t.length;
-    });
+function showNextTab(isFirstCycle = false) { 
 
-    chrome.tabs.query({
-        currentWindow: true, 
-        highlighted: true
-    }, function(t){
-        var currentIdx = t[0].index;
-        var nextIdx = currentIdx === ntabs - 1 ? 0 : currentIdx + 1
-        chrome.tabs.highlight({ 'tabs': nextIdx });
-    });
+    // break out infinite loop
+    if (!session.isPlaying) return;
+
+    session.timerId = setTimeout(function(){
+        chrome.tabs.query({
+            windowId: session.windowId,
+        }, function(tabs){
+            var t = tabs.find(function(e) {
+                if (e.highlighted) return true;
+            });
+            var nextIndex =  t.index + 1 >= tabs.length ? 0 : t.index + 1;
+            console.log("switch to next tab");
+            chrome.tabs.highlight({ 'tabs': nextIndex });
+        });
+        setTimeout(() => showNextTab(false), session.interval * 1000)
+    }, session.interval * 1000);
+}
+
+function pause() {
+    clearTimeout(session.timerId);
+    session.isPlaying = false;
+}
+
+function play() {
+    session = newSessionObject();
+    session.isPlaying = true;
+    session.windowId = chrome.windows.WINDOW_ID_CURRENT;
+    showNextTab(true)
+}
+
+chrome.browserAction.onClicked.addListener(function(){
+    session.isPlaying ? pause() : play()
 });
